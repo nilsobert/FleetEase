5ef86c7906a19d7c21034770e3c88fdcea5df435
Julian <juwalu02@gmail.com>
2024-11-23 16:09:08 +0100

    added main

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from api.api import API
+import algo
+import fleetStatistics
+import applicationStatistics
+import server
+import threading
+import time
+
+def run_server():
+    app = server.create_app()
+    app.run(port=5000, use_reloader=False)
+
+def start_algorithm():
+    pass
+
+def calculate_stats():
+    pass
+
+def main():
+    server_thread = threading.Thread(target=run_server, daemon=True)
+    server_thread.start()
+
+    print("Server started. Main program running...")
+    
+    try:
+        while True:
+            start_algorithm()
+            calculate_stats()
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Shutting down main program.")
+
+
+if __name__ == "__main__":
+    main()
--- src/server.py
+++ src/server.py
@@ -1,35 +1,46 @@
 import json
+import time
 from flask import Flask, jsonify, request
-
 from applicationStatistics import getApplicationStatistics
 from fleetStatistics import getFleetStatistics
 
-app = Flask(__name__)
+def create_app():
+    """Factory function to create the Flask app."""
+    app = Flask(__name__)
 
-vehicles = [1, 2, 3, 4, 5]
-customers = []
-fleet_statistics = []
-application_statistics = []
+    vehicles = [1, 2, 3, 4, 5]
+    customers = []
+    fleet_statistics = []
+    application_statistics = []
 
-@app.route('/vehicles', methods=['GET'])
-def get_vehicles():
- return jsonify(vehicles)
+    @app.route('/vehicles', methods=['GET'])
+    def get_vehicles():
+        return jsonify(vehicles)
 
-@app.route('/customers', methods=['GET'])
-def get_customers():
- return jsonify(customers)
+    @app.route('/customers', methods=['GET'])
+    def get_customers():
+        return jsonify(customers)
 
-@app.route('/fleetStatistics', methods=['GET'])
-def get_fleet_statistics():
- return jsonify(getFleetStatistics())
+    @app.route('/fleetStatistics', methods=['GET'])
+    def get_fleet_statistics():
+        return jsonify(getFleetStatistics())
 
-@app.route('/applicationStatistics', methods=['GET'])
-def get_application_statistics():
- return jsonify(getApplicationStatistics(5))
+    @app.route('/applicationStatistics', methods=['GET'])
+    def get_application_statistics():
+        return jsonify(getApplicationStatistics(5))
 
-@app.route('/updateParameters', methods=['PUT'])
-def update_parameters():
- return jsonify({'message': 'parameters updated'})
+    @app.route('/updateParameters', methods=['PUT'])
+    def update_parameters():
+        return jsonify({'message': 'parameters updated'})
 
-if __name__ == '__main__':
- app.run(port=5000)
+    @app.route('/test_streaming')
+    def test_streaming():
+        def generate():
+            for i in range(100000):
+                time.sleep(1)
+                # Yield a JSON object as a string
+                yield json.dumps({"value": i}) + "\n"
+
+        return app.response_class(generate(), mimetype='application/json')
+
+    return app
