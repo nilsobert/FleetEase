a5b64cda1259767fb6fe9934b1b03dc750ce142f
Zimbule <nico.stengel@outlook.de>
2024-11-23 16:07:01 +0100

    Erster Versuch Streams in Angluar

--- src/FleetEase_Frontend/src/main.ts
+++ src/FleetEase_Frontend/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent,{
+  providers:[provideHttpClient()],
+} )
   .catch((err) => console.error(err));
