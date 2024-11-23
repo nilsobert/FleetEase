4558aebb1dfca19d30422213cacb3bd9f2e5dece
nilsobert <nh123.1@gmx.de>
2024-11-23 11:13:25 +0100

    Refined fleet statistics calculations and rounding

--- src/fleetStatistics.py
+++ src/fleetStatistics.py
@@ -1,17 +1,18 @@
 consumption_in_kwh_per_km = 16
 velocity_in_km_per_hour = 40
+rounding_decimals_for_ratios = 2
 
 # in kilometer: number($float)
 def get_total_distance_travelled():
-    return 0
+    return 40
 
 # in minutes
 def get_time_passed():
-    return 0
+    return 90
 
 # in kilometer: number($float)
 def get_total_distance_finished_rides():
-    return 0
+    return 30
 
 def get_amount_of_customers_waiting():
     return 0
@@ -27,5 +28,9 @@ def getStatistics():
         raise Exception("Car travelled faster than velocity is set.")
     total_consumption = get_total_distance_travelled()*consumption_in_kwh_per_km
     productive_ratio = get_total_distance_finished_rides()/get_total_distance_travelled() # ratio of productive driving
-    idle_ratio = get_total_distance_travelled/(get_time_passed()/60*velocity_in_km_per_hour)
-    return {"Total consumption": total_consumption, "Productive ratio": productive_ratio, "Idle ratio": idle_ratio, "Amount of customers waiting": get_amount_of_customers_waiting()}
+    idle_ratio = get_total_distance_travelled()/(get_time_passed()/60*velocity_in_km_per_hour)
+    return {"Total consumption (kWh)": total_consumption, "Productive ratio": round(productive_ratio,rounding_decimals_for_ratios),
+            "Idle ratio": round(idle_ratio,rounding_decimals_for_ratios), "Amount of customers waiting": get_amount_of_customers_waiting()}
+
+if __name__ == "__main__":
+    print(getStatistics())
