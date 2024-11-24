ff03818d8b21773cc2a0c984492424bcac9d361a
dominjack <schiwietzdominik0@gmail.com>
2024-11-24 01:25:40 +0100

    server

--- src/main.py
+++ src/main.py
@@ -4,35 +4,10 @@ import time
 from algo import Assigner
 from api.api import API
 import asyncio
-from synchronizer import Synchronizer
+from server import app, synchronizer, data_lock
 
-app = Flask(__name__)
 
-# Shared data and lock
-synchronizer = Synchronizer()
-data_lock = threading.Lock()
-
-# Worker thread function
-def assignement_worker(scenario, vehicles, customers, api, algorithm="basic"):
-    global synchronizer
-    assigner = Assigner(scenario=scenario, vehicles=vehicles, customers=customers, api=api, algorithm=algorithm)
-    assigner.assign(synchronizer, data_lock)
-    time.sleep(1)  # Prevent busy-waiting
-
-# Flask route to update shared data
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    global synchronizer
-    data = request.json
-    message = data.get("message")
-    
-    if not message:
-        return jsonify({"error": "Message is required"}), 400
-    
-    
-    return jsonify({"status": "Message received"}), 200
-
-def init():
+def init_api():
     print("Initializing api...")
     api = API()
     num_vehicles = 30
@@ -42,13 +17,20 @@ def init():
     print("    finished")
     return scenario, vehicles, customers, api
 
+# Worker thread function
+def assignement_worker(scenario, vehicles, customers, api, algorithm="basic"):
+    global synchronizer
+    assigner = Assigner(scenario=scenario, vehicles=vehicles, customers=customers, api=api, algorithm=algorithm, debug=False)
+    assigner.assign(synchronizer, data_lock)
+    time.sleep(1)  # Prevent busy-waiting
+
 
 if __name__ == "__main__":
     scenario, vehicles, customers, api = init()
     # Start the worker thread
     thread = threading.Thread(target=assignement_worker, args=(scenario, vehicles, customers, api))
     thread.start()
-    time.sleep(2)
+    time.sleep(5)
 
     # Start the Flask server
     app.run(debug=True)
